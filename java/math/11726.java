7394a2f3a80bcf21315b2d3059aac74b2a532ebe
ehdgh <ehdgh25@naver.com>
2022-07-15 19:58:24 +0900

    11726 - 2xn 타일링

---
+++ java/math/11726.java
@@ -0,0 +1,26 @@
+/*
+ * 11726 - 2xn 타일링
+ */
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+
+public class Main {
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        int n = Integer.parseInt(br.readLine());
+        // n = 1 일 때, arr[2]가 할당되어 있도록 n + 1 이 아닌 n + 2
+        int[] arr = new int[n + 2];
+        arr[1] = 1;
+        arr[2] = 2;
+
+        for (int i = 3; i <= n; i++) {
+            arr[i] = (arr[i - 1] + arr[i - 2]) % 10007;
+        }
+
+        System.out.println(arr[n]);
+        br.close();
+    }
+}
