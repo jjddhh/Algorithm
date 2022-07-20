8b1349fbbed882c91239f2fe82c9de8dceb70841
ehdgh <ehdgh25@naver.com>
2022-07-20 20:31:32 +0900

    16194 - 카드 구매하기2

---
+++ java/dp/16194.java
@@ -0,0 +1,39 @@
+/*
+ * 16194 - 카드 구매하기 2
+ */
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+
+public class Main {
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st;
+
+        int n = Integer.parseInt(br.readLine());
+
+        int[] dp = new int[n + 1];
+        int[] arr = new int[n + 1];
+
+        Arrays.fill(dp, 10000001);
+        dp[0] = 0;
+
+        st = new StringTokenizer(br.readLine());
+        int count = st.countTokens();
+        for (int i = 1; i <= count; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        for (int i = 1; i <= n; i++) {
+            for (int sub_i = 1; sub_i <= i; sub_i++) {
+                dp[i] = Math.min(dp[i], dp[i - sub_i] + arr[sub_i]);
+            }
+        }
+
+        System.out.println(dp[n]);
+        br.close();
+    }
+}
