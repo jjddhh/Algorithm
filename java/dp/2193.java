baf599edbe120cf8d6f34e45c023ff7280418de3
ehdgh <ehdgh25@naver.com>
2022-07-22 21:46:25 +0900

    2193 - 이친수

---
+++ java/dp/2193.java
@@ -0,0 +1,33 @@
+/*
+ * 2193 - 이친수
+ */
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+
+public class Main {
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        int n = Integer.parseInt(br.readLine());
+
+        long[][] dp = new long[91][2];
+        dp[1][1] = 1;
+
+        for (int i = 2; i <= n; i++) {
+            for (int sub_i = 0; sub_i <= 1; sub_i++) {
+                if(sub_i == 1) dp[i][1] = dp[i - 1][0];
+                else if(sub_i == 0) dp[i][0] = dp[i - 1][0] + dp[i - 1][1];
+            }
+        }
+        
+        long sum = 0;
+        for (int i = 0; i <= 1; i++) {
+            sum += dp[n][i];
+        }
+
+        System.out.println(sum);
+        br.close();
+    }
+}
