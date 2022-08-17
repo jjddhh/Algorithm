4c6cbf61a31c73b990be3aea8ef94564a9f88c0c
imdh <ehdgh25@naver.com>
2022-08-17 10:24:36 +0900

    [Silver III] Title: N과 M (6), Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/15655. N과 M （6）/N과 M （6）.cc
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int N, M;
+int bt[8];
+int input[8];
+bool is_used[8];
+
+void func(int k)
+{
+	if (k == M) {
+		for (int i = 0; i < M; i++) {
+			cout << bt[i] << " ";
+		}
+		cout << '\n';
+		return;
+	}
+
+	for (int i = 0; i < N; i++) {
+		if (k == 0 || (!is_used[i] && bt[k - 1] < input[i])) {
+			bt[k] = input[i];
+			is_used[i] = 1;
+			func(k + 1);
+			is_used[i] = 0;
+		}
+		
+	}
+}
+
+
+
+int main() 
+{
+	cin >> N >> M;
+
+	for (int i = 0; i < N; i++) {
+		cin >> input[i];
+	}
+
+	sort(input, input + N);
+	func(0);
+
+
+	return 0;
+}
+
+
