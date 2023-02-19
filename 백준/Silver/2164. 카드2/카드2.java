7d41857aa794aa2b6d455ab9811137e696ec7873
imdh <eastlake520@gmail.com>
2023-02-19 20:51:06 +0900

    [Silver IV] Title: 카드2, Time: 196 ms, Memory: 45700 KB -BaekjoonHub

---
+++ 백준/Silver/2164. 카드2/카드2.java
@@ -0,0 +1,30 @@
+import java.awt.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+class Main {
+
+    private static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    private static StringBuilder sb = new StringBuilder();
+    private static StringTokenizer st;
+
+    private static int n;
+
+    public static void main(String[] args) throws IOException {
+
+        n = Integer.parseInt(br.readLine());
+        Queue<Integer> queue = new LinkedList<>();
+        for (int i = 1; i <= n; i++) {
+            queue.add(i);
+        }
+
+        while (queue.size() > 1) {
+            queue.poll();
+            queue.add(queue.poll());
+        }
+
+        System.out.println(queue.poll());
+    }
+}
