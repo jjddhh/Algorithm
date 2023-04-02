e4fc5eaeb6c8abc6eb209dcaa31b9152973da52a
imdh <eastlake520@gmail.com>
2023-04-02 14:23:49 +0900

    [level 3] Title: 단어 변환, Time: 0.34 ms, Memory: 76.6 MB -BaekjoonHub

---
+++ 프로그래머스/lv3/43163. 단어 변환/단어 변환.java
@@ -0,0 +1,47 @@
+import java.util.*;
+
+class Solution {
+    public int solution(String begin, String target, String[] words) {
+        int word_len = begin.length();
+        boolean[] visit = new boolean[words.length];
+        Queue<Word> que = new LinkedList<>();
+        que.add(new Word(begin, 0));
+        
+        int answer = 0;
+        while(!que.isEmpty()) {
+            Word cur = que.poll();
+            if(cur.w.compareTo(target) == 0) {
+                answer = cur.t;
+                break;
+            }
+            
+            for(int i = 0; i < words.length; i++) {
+                if(visit[i]) continue;
+                
+                int cnt = 0;
+                for(int j = 0; j < word_len; j++) {
+                    Character a = cur.w.charAt(j);
+                    Character b = words[i].charAt(j);
+                    if(a.compareTo(b) != 0) cnt++;
+                }
+                
+                if(cnt == 1) {
+                    visit[i] = true;
+                    que.add(new Word(words[i], cur.t + 1));
+                }
+            }
+        }
+        
+        return answer;
+    }
+    
+    class Word {
+        String w;
+        int t;
+        
+        Word (String w, int t) {
+            this.w = w;
+            this.t = t;
+        }
+    }
+}
